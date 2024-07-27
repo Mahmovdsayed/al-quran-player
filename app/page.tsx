1b829da55a1f7c3ceb06d66d3303fdf4a48f43f6
Mahmoud Sayed <138359247+Mahmovdsayed@users.noreply.github.com>
2024-07-27 23:49:15 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { FaGithub } from "react-icons/fa";
 
 export default function Home() {
   return <>
-    <main className="min-h-screen flex items-center justify-center flex-col px-4">
+    <main className="min-h-screen flex items-center justify-center flex-col px-4 text-center">
       <Chip as={Link} showAnchorIcon href={'https://github.com/Mahmovdsayed'} target={"_blank"} startContent={<FaGithub />} size="sm" radius="sm">Mahmovdsayed</Chip>
       <h1 className="text-4xl md:text-6xl font-semibold uppercase mb-4 mt-2">
         Nextjs Template
